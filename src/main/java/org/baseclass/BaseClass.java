70597f9d52e77e1c92b6afa5247511b91e149021
AravinthBabu1998 <aravinthbabu.er@gmail.com>
2021-10-21 12:08:02 +0530

    Project Structure

---
+++ src/main/java/org/baseclass/BaseClass.java
@@ -0,0 +1,9 @@
+package org.baseclass;
+
+public class BaseClass {
+	public static void main(String[] args) {
+		System.out.println("aravinth");
+		System.out.println("babu");
+	}
+
+}
---
+++ src/main/java/org/job/Job.java
@@ -0,0 +1,8 @@
+package org.job;
+
+public class Job {
+	public static void main(String[] args) {
+		System.out.println("Automation Tester");
+	}
+
+}
